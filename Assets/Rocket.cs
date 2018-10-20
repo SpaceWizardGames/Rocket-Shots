142f86b4b9bbb0e70846adc173c30d973a548270
Trancefate <44308629+Trancefate@users.noreply.github.com>
2018-10-19 22:26:52 -0400

    Second Commit
    Setup .gitignore with commonly ignored unity files.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+   [SerializeField] float rcsThrust = 200f;
+   [SerializeField] float mainThrust = 50f;
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+		}
+
+    // Update is called once per frame
+    void Update() {
+        Thrust();
+        Rotate();	
+	}
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (audioSource.isPlaying == false)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // manually control rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false;
+    }
+}
